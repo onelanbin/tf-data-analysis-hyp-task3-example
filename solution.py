6d28fd30c25dea967ac9218a1a7405097b3f65fa
onelanbin <130176903+onelanbin@users.noreply.github.com>
2023-04-13 17:26:23 +0300

    Task3HYP Done

--- solution.py
+++ solution.py
@@ -1,14 +1,14 @@
 import pandas as pd
 import numpy as np
-
+import scipy.stats as stats
 
 chat_id = 1266169265 # Ваш chat ID, не меняйте название переменной
 
-import scipy.stats as stats
-
-def test_NPV(NPV_control: list, NPV_test: list, alpha: float = 0.07) -> bool:
+def solution(NPV_control: list, NPV_test: list, alpha: float = 0.07) -> bool:
     t_stat, p_val = stats.ttest_ind(NPV_control, NPV_test, equal_var=False)
     if(p_val < alpha and t_stat > 0):
         return True
     else:
         return False
+
+
