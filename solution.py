e49b44153ee514dc4145a2387b9ee7de91623379
onelanbin <130176903+onelanbin@users.noreply.github.com>
2023-04-13 15:32:35 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,13 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 1266169265 # Ваш chat ID, не меняйте название переменной
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+import scipy.stats as stats
+
+def test_NPV(NPV_control: list, NPV_test: list, alpha: float = 0.07) -> bool:
+    t_stat, p_val = stats.ttest_ind(NPV_control, NPV_test, equal_var=False)
+    if(p_val < alpha and t_stat > 0):
+        return True
+    else:
+        return False
